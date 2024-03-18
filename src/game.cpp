de8a33f32e715b5740c5f634ac6a61a1bfcb61fc
Chrd26 <christosdouris@outlook.com>
2024-03-18 20:03:24 +0200

    Added created a simple window and fount out that SDL3 documentation actually exists

--- src/game.cpp
+++ src/game.cpp
@@ -1,21 +1,63 @@
 #include "game.h"
 
+const int Game::height = 720;
+const int Game::width = 1280;
+SDL_Window *Game::window = nullptr;
+
 bool Game::Initialise()
 {
-    if (SDL_INIT(SDL_INIT_VIDEO|SDL_INIT_AUDIO) < 0)
+	// Make sure that video and audio have been initialised
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
     {
         std::cout << "Failed to initialise SDL" << std::endl;
         return false;
     }
+    
+    // Create Window
+    window = SDL_CreateWindow("Rebuild Back Better",
+							  width,
+							  height,
+							  0);
+							  
+	// Set window position
+	int windowPOSOutput = SDL_SetWindowPosition(window,
+												SDL_WINDOWPOS_CENTERED,
+												SDL_WINDOWPOS_CENTERED);
+							  
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create window" << std::endl;
+		return false;	
+	}
 
     return true;
 }
 
 Game::Game()
 {
-    if (!Initialise)
+    if (!Initialise())
     {
         exit(-1);
     }
+    
     std::cout << "Start Game" << std::endl;
+    
+    SDL_Event events;
+    bool quit = false;
+    
+    while(!quit)
+    {
+		while(SDL_PollEvent(&events))
+		{
+			if (events.type == SDL_EVENT_QUIT)
+			{
+				quit = true;
+			}
+		}	
+	}
+	
+	
+	SDL_DestroyWindow(window);
+	window = nullptr;
+	SDL_Quit();
 }
--- src/game.h
+++ src/game.h
@@ -1,12 +1,17 @@
 #include <SDL.h>
 #include <iostream>
 
-class Game{
+class Game
+{
 public:
     Game();
 
 // Game Initialisation
 private:
+	static const int height;
+	static const int width;
+	
+	static SDL_Window *window;
     static bool Initialise();
 
 };
