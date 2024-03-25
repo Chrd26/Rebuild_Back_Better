b40993b4a8a0ee8ab1ee63e1ccb1b16216afe14f
Chrd26 <christosdouris@outlook.com>
2024-03-25 21:57:48 +0200

    Removed all objective c++ files and updated cmake

--- src/macos.mm
+++
@@ -1,8 +0,0 @@
-#import "macos.h"
-
-@implementation MacBuild
--	(void) InitialiseProperties{
-		self.standardBundle = [NSBundle mainBundle];
-		self.findFontMenuTitle = [_standardBundle pathForResource:@"/fonts/ArianVioleta-dz2K" ofType:@"ttf"];
-	}
-@end
