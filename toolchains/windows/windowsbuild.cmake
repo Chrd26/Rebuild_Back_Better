e48b958177153f4579157a483146e60b6a9fa993
Chrd26 <christosdouris@outlook.com>
2024-04-06 23:36:06 +0300

    Update

--- toolchains/windows/windowsbuild.cmake
+++ toolchains/windows/windowsbuild.cmake
@@ -1,2 +1,12 @@
-et(CMAKE_SYSTEM_NAME Darwin)
-set(CMAKE_CXX_COMPILER MSVC)
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_CXX_COMPILER clang++)
+
+# where is the target environment located
+set(CMAKE_FIND_ROOT_PATH /usr/local/Cellar/llvm/17.0.6_1/bin/clang-cl)
+# adjust the default behavior of the FIND_XXX() commands:
+# search programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+# search headers and libraries in the target environment
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
